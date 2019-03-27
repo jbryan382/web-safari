eb58be5eddb8ab748a81e37cb3d46324d39f2fbd
Jordan Bryan <jbryan382@gmail.com>
2019-03-27 16:07:46 -0400

    Fixed location and working on delete

--- src/components/Animals.js
+++ src/components/Animals.js
@@ -3,11 +3,14 @@ import axios from 'axios'
 
 class Animals extends Component {
   state = {
-    results: [],
-    animalId: 0,
-    animalSpecies: '',
-    animalCount: 0,
-    animalLocation: ''
+    animals: [],
+    location: '',
+    animalLocation: [],
+    deleteLocation: [],
+    totalNumberOfAnimals: '',
+    animalsToCount: [],
+    lionTigerBear: '',
+    targetAnimals: []
   }
 
   componentDidMount() {
@@ -15,9 +18,38 @@ class Animals extends Component {
       console.log({ resp })
 
       this.setState({
-        results: resp.data
+        animals: resp.data
       })
     })
+    this.getJungleAnimals()
+  }
+
+  getJungleAnimals = () => {
+    axios
+      .get(`https://localhost:5001/api/Animals/location/jungle`)
+      .then(resp => {
+        console.log({ resp })
+        this.setState({
+          animalLocation: resp.data
+        })
+      })
+  }
+
+  // countAnimals = () => {
+  //   let count = 0;
+  //   let LTBCount = 0;
+
+  // }
+
+  deleteLocation = () => {
+    axios
+      .delete(`http://localhost:5001/api/Animals/location/desert`)
+      .then(resp => {
+        console.log({ resp })
+        this.setState({
+          deleteLocation: resp.data
+        })
+      })
   }
 
   render() {
@@ -27,23 +59,24 @@ class Animals extends Component {
         <section>
           <h2>Be careful of all those animals out there:</h2>
           <ul>
-            {this.state.results.map((animal, i) => {
-              return <li key={i}>{this.state.results[i].species}</li>
+            {this.state.animals.map((animal, i) => {
+              return <li key={i}>{this.state.animals[i].species}</li>
             })}
           </ul>
           <section>
             <h2>Especially in the Jungle:</h2>
+            {/* <button onClick={this.findByLocation}>Enter</button> */}
             <ul>
-              {this.state.results.map((animal, i) => {
-                return (
-                  <li key={i}>
-                    {this.state.results[i].species
-                      ? this.state.results[i].locationOfLastSeen === 'Jungle'
-                      : ''}
-                  </li>
-                )
+              {this.state.animalLocation.map((animal, i) => {
+                return <li key={i}>{this.state.animalLocation[i].species}</li>
               })}
             </ul>
+          </section>
+
+          <section>
+            <button onClick={this.deleteLocation}>
+              Who needs Desert Animals... right?
+            </button>
           </section>
         </section>
       </>
